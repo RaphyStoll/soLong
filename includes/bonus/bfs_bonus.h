de277ba0058d7ea6eef5af564b7e7d56b9a5087d
RaphyStoll <raphael.ferreira@ik.me>
2025-01-14 11:13:15 +0100

    swap pc

--- includes/bonus/bfs_bonus.h
+++ includes/bonus/bfs_bonus.h
@@ -1,14 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   bfs_bonus.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: raphaelferreira <raphaelferreira@studen    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/01/13 16:50:09 by raphaelferr       #+#    #+#             */
+/*   Updated: 2025/01/13 16:56:15 by raphaelferr      ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #ifndef BSF_BONUS_H
 # define BSF_BONUS_H
 
-#include "game_bonus.h"
-#include "macro_bonus.h"
+# include "game_bonus.h"
+# include "macro_bonus.h"
 
 typedef struct s_bfs
 {
-	int		width;    		//Largeur de la map
-	int		height;   		//Hauteur de la map
-	char	**map;			//copy de la map pour eviter tout changent sur la map jouable
+	int		width;
+	int		height;
+	char	**map;
 	int		**dist;
 	int		*queue_x;
 	int		*queue_y;
